44cae298a6eabb6f05dafc5a19a0c2969d6d1dfe
DalaiLomo <reisereise.85@gmail.com>
2021-09-17 13:49:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProjectNF
+
+Developed with Unreal Engine 4
---
+++ Source/ProjectNF/ProjectNFGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectNFGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTNF_API AProjectNFGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
