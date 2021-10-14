7f67aaaab35e9060300a859aa53a7643f7448f80
DalaiLomo <reisereise.85@gmail.com>
2021-10-14 08:11:24 +0200

    Implement basic player movement

--- Source/ProjectNF/PlayerCharacter.cpp
+++ Source/ProjectNF/PlayerCharacter.cpp
@@ -3,8 +3,20 @@
 
 #include "PlayerCharacter.h"
 
+/******************************************************* Constructor and engine functions */
+
 // Sets default values
-APlayerCharacter::APlayerCharacter()
+APlayerCharacter::APlayerCharacter() : 
+	// Base rates
+	BaseTurnRate(45.f),
+	BaseLookupRate(45.f),
+	// Mouselook sensitivity scale factors
+	MouseHipTurnRate(1.0f),
+	MouseHipLookupRate(1.0f),
+	MouseAimingTurnRate(0.4f),
+	MouseAimingLookupRate(0.4f),
+	// true when aiming
+	bAiming(false)
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -25,10 +37,104 @@ void APlayerCharacter::Tick(float DeltaTime)
 
 }
 
+
+/******************************************************* Input bindings */
+
 // Called to bind functionality to input
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	check(PlayerInputComponent);
 
+	// We have to make sure that string name matches the name of the input mapping in project settings!
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("TurnRate", this, &APlayerCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &APlayerCharacter::LookUpAtRate);
+	PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::Turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::Lookup);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	/*
+	PlayerInputComponent->BindAction("FireButton", IE_Pressed, this, &APlayerCharacter::FireButtonPressed);
+	PlayerInputComponent->BindAction("FireButton", IE_Released, this, &APlayerCharacter::FireButtonReleased);
+
+	PlayerInputComponent->BindAction("AimingButton", IE_Pressed, this, &APlayerCharacter::AimingButtonPressed);
+	PlayerInputComponent->BindAction("AimingButton", IE_Released, this, &APlayerCharacter::AimingButtonReleased);
+	*/
 }
 
+
+/******************************************************* Movement and mouse look */
+
+void APlayerCharacter::MoveForward(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		// find which way is forward
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0, Rotation.Yaw, 0 };
+
+		const FVector Direction{ FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::X) };
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void APlayerCharacter::MoveRight(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		// find which way is right
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0, Rotation.Yaw, 0 };
+
+		const FVector Direction{ FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::Y) };
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void APlayerCharacter::TurnAtRate(float Rate)
+{
+	// Calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds()); // deg/sec * sec/frame
+}
+
+void APlayerCharacter::LookUpAtRate(float Rate)
+{
+	// Calculate delta for this frame from the rate information
+	AddControllerPitchInput(Rate * BaseLookupRate * GetWorld()->GetDeltaSeconds()); // deg/sec * sec/frame
+}
+
+void APlayerCharacter::Turn(float Value)
+{
+	float TurnScaleFactor{};
+
+	if (bAiming)
+	{
+		TurnScaleFactor = MouseAimingTurnRate;
+	}
+	else
+	{
+		TurnScaleFactor = MouseHipTurnRate;
+	}
+
+	AddControllerYawInput(Value * TurnScaleFactor);
+}
+
+void APlayerCharacter::Lookup(float Value)
+{
+	float LookupScaleFactor{};
+
+	if (bAiming)
+	{
+		LookupScaleFactor = MouseAimingLookupRate;
+	}
+	else
+	{
+		LookupScaleFactor = MouseHipLookupRate;
+	}
+
+	AddControllerPitchInput(Value * LookupScaleFactor);
+}
--- Source/ProjectNF/PlayerCharacter.h
+++ Source/ProjectNF/PlayerCharacter.h
@@ -26,4 +26,64 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	/** Base turn rate, in deg/sec. Other scaling may affect turn rate */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float BaseTurnRate;
+
+	/** Base look up/down rate in deg/sec. Other scaling may affect turn rate */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float BaseLookupRate;
+
+	/** True when aiming */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	bool bAiming;
+
+	/** Scale factor from mouse look sensitivity. Lookup rate when aiming */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"), meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
+	float MouseAimingTurnRate;
+
+	/** Scale factor from mouse look sensitivity. Lookup rate when aiming */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"), meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
+	float MouseAimingLookupRate;
+
+	/** Scale factor from mouse look sensitivity. Turn rate when not aiming */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"), meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
+	float MouseHipTurnRate;
+
+	/** Scale factor from mouse look sensitivity. Turn rate when not aiming */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"), meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
+	float MouseHipLookupRate;
+
+protected:
+	/** Called for forward/backwards input */
+	void MoveForward(float Value);
+
+	/** Called for side to side input */
+	void MoveRight(float Value);
+
+	/**
+	* Called via input to turn at a given rate
+	* @param Rate	This is a normalized rate. i.e. 1.0 means 100% of desired turn rate
+	*/
+	void TurnAtRate(float Rate);
+
+	/**
+	* Called via input to look up/down at a given rate
+	* @param Rate	this is a normalized rate. i.e. 1.0 means 100% of desired rate
+	*/
+	void LookUpAtRate(float Rate);
+
+	/**
+	* Rotate controller on mouse X movement
+	* @param Value	The input value from mouse movement
+	*/
+	void Turn(float Value);
+
+	/**
+	* Rotate controller on mouse Y movement
+	* @param Value	The input value from mouse movement
+	*/
+	void Lookup(float Value);
+
 };
