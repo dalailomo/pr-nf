01f4037fcd8596da6d4965ad9580affdcf918a08
DalaiLomo <reisereise.85@gmail.com>
2021-10-19 08:42:18 +0200

    tweak head bobbing values

--- Source/ProjectNF/CameraHeadBobbing.cpp
+++ Source/ProjectNF/CameraHeadBobbing.cpp
@@ -7,8 +7,8 @@ UCameraHeadBobbing::UCameraHeadBobbing()
 {
     OscillationDuration = 1.0f;
     OscillationBlendInTime = 1.0f;
-    OscillationBlendOutTime = 1.0f;
+    OscillationBlendOutTime = 4.0f;
 
-    LocOscillation.Z.Amplitude = 2.5f;
+    LocOscillation.Z.Amplitude = 3.0f;
     LocOscillation.Z.Frequency = 10.0f;
 }
